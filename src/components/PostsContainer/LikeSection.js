63de7112bfb3bd321c4356d2563f3ff162f14144
Reagan Barrington <reagan@barringtonfamily.net>
2020-02-18 16:58:11 -0500

    likes increment

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -4,6 +4,9 @@
 import React from "react";
 
 const LikeSection = props => {
+
+  console.log(props, " props in LikeSection")
+
   return (
     <div>
     <div
@@ -11,15 +14,13 @@ const LikeSection = props => {
       key="likes-icons-container"
     >
       <div className="like-section-wrapper">
-        <i className="far fa-heart" />
+        <i onClick = {props.incrementLikes} className="far fa-heart" />
       </div>
       <div className="like-section-wrapper">
         <i className="far fa-comment" />
       </div>
     </div>
-    <p className="like-number">
-      
-      likes</p>
+    <p className="like-number"> {props.likes} likes</p>
 </div>
   )
 };
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -4,14 +4,19 @@ import React, {useState} from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
-import dummyData from ".../dummyData";
+import dummyData from "../../dummy-data";
 
 const PostsPage = () => {
   // set up state for your data
   const [data] = useState(dummyData);
+
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {data.map(p => (
+        // pass data as props to Post
+        <Post propsPassedFromParent={p} key={p.username} />
+      ))}
     </div>
   );
 };
