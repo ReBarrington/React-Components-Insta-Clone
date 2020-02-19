e1d004d70dc21a9eaa34e38974b7b948880630bd
Reagan Barrington <reagan@barringtonfamily.net>
2020-02-19 11:44:28 -0500

    Oops. Just noticed I didnt do the comment section

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -5,11 +5,18 @@ import Comment from "./Comment";
 import "./Comment.css";
 
 const CommentSection = props => {
+  console.log(props, " is props in CommentSection")
+
   // Add state for the comments
+  const [comment] = useState(props.comments)
+  console.log(comment, " is comment from CommentSection")
 
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
+      {comment.map(c => (
+         <Comment comment={c.text} key={c.username}/>
+      ))}
       <CommentInput />
     </div>
   );
